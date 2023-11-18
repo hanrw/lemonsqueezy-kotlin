944b0347f7357bff64a710b2de4756e71fa4ab98
slam <slamhan1987@gmail.com>
2023-11-18 19:50:55 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+rootProject.name = "lemonsqueezy-kotlin"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        
+    }
+}
+
+include(":shared")
