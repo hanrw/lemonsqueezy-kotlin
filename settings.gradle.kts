a25209f0f3f48cb4b8c68d92c93c5c83727e75ca
slam <slamhan1987@gmail.com>
2024-01-31 20:25:37 +0800

    chore: add mavenLocal for plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        
+        mavenLocal()
         google()
         gradlePluginPortal()
         mavenCentral()
